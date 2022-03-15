2d6db9e12f1f644768d2a584776e5804757d31f1
Stephen Gray <stephen_gray1255@yahoo.com>
2022-03-15 12:37:24 +1100

    c2

---
+++ Challenges/c2_control_flow_extended/Challenge.cs
@@ -0,0 +1,113 @@
+﻿using System;
+namespace Challenges.c2_control_flow_extended
+{
+    public class Challenge
+    {
+        // -------------- FOUNDATION --------------
+
+        /// <summary>
+        ///     Write a method that concatenates two strings together with a space in between them.
+        /// </summary>
+        /// <param name="firstName"> the first name of a person </param>
+        /// <param name="lastName"> the last name of a person </param>
+        /// <returns> a string which is the concatenation of the 2 parameters </returns>
+        public string GetFullName(string firstName, string lastName)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        ///     Write a method that checks if a number is within 0 and a given range. Both inclusive.
+        /// </summary>
+        /// <param name="number"> the number to find within the range </param>
+        /// <param name="rangeLimit"> the limit of the range </param>
+        /// <returns> a boolean based on whether the number is between 0 and the range limit </returns>
+        public bool IsWithinRange(int number, int rangeLimit)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     Write a method that takes two numbers and an operator. Based on the operator preform the
+        ///     equation and return the result.
+        ///     e.g 10, 2 , "/" = 5
+        ///     e.g 10, 2 , "+" = 12
+        ///
+        ///     If either number is below 0 return -1
+        ///     If an operator is not "*", "/", "+", "-" return -1
+        /// </summary>
+        /// <param name="numberOne"> the first number in the equation </param>
+        /// <param name="numberTwo"> the second number in the equation </param>
+        /// <param name="op"> the operator to complete the equation </param>
+        /// <returns> the result of the equation </returns>
+        public int StringCalculator(int numberOne, int numberTwo, String op)
+        {
+            throw new NotImplementedException();
+        }
+
+        // -------------- INTERMEDIATE --------------
+
+        /// <summary>
+        ///     We have a dog that likes to bark. We need to wake up if the dog is barking at night!
+        ///     This method should return true or false if we need to wake up or not.
+        ///
+        ///     If the dog is barking before 8 or after 22 hours we need to wake up.
+        ///     All other cases we do not need to wake up.
+        ///     If the hourOfDay is not within 0 - 23 we do not need to wake up.
+        /// </summary>
+        /// <param name="barking"> boolean it represents if our dog is currently barking or not. </param>
+        /// <param name="hourOfDay"> the hour of the day and is of type int with the name hourOfDay and has a valid range of 0-23. </param>
+        /// <returns> If we need to wake up or not based on the conditions above. </returns>
+        public bool ShouldWakeUp(bool barking, int hourOfDay)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     Write a method to display the middle character or characters of a word.
+        ///     If multiple words separated with a " " or no words are given it should return "Invalid Input"
+        /// </summary>
+        /// <param name="word"> the word that we want to check </param>
+        /// <returns> the middle character or characters... for example, passing in "Sam" would return "The middle character in the string: a"</returns>
+        public string GetMiddleCharacter(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     Write a method called printMegaBytesAndKiloBytes that has 1 parameter of type int.
+        ///     The method needs to see how many megabytes are in the given kilobyte and work out the remainder
+        ///     Then it needs to return a message in the format "XX KB = YY MB and ZZ KB".
+        ///
+        ///     If the kiloBytes param is less than 0 return "Invalid value"
+        ///     1 megaByte = 1024 kiloBytes
+        /// </summary>
+        /// <param name="kiloBytes"> the number of kilobytes to be converted </param>
+        /// <returns> a string in the format above if the input is valid, or "Invalid value" </returns>
+        public string PrintMegaBytesAndKiloBytes(int kiloBytes)
+        {
+            throw new NotImplementedException();
+        }
+
+        // -------------- ADVANCED --------------
+
+        /// <summary>
+        ///     Write a method to compute the future investment value returned at a given interest rate for a specified number
+        ///     of years.
+        ///     It should return a string the matches this format "You now have RESULT" the RESULT needs to be rounded.
+        ///
+        ///     If initialInvestment is less than or equal 0 return "Invalid investment"
+        ///     If years is less than or equal 0 return "Invalid years"
+        ///     If rateOfInterest is less than 0 or greater than or equal to 1 return "Invalid rate of interest"
+        ///
+        ///     HINT: https://www.w3schools.com/java/java_for_loop.asp
+        /// </summary>
+        /// <param name="initialInvestment"> the starting value </param>
+        /// <param name="years"> the number of years that the investment will take </param>
+        /// <param name="rateOfInterest"> the rate at which, each year, the investment will increase by </param>
+        /// <returns> the total amount after x number of years (including the initial investment) </returns>
+        public string CalculateReturn(int initialInvestment, int years, double rateOfInterest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests/Test_c2.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Challenges.c2_control_flow_extended;
+
+namespace Tests
+{
+    [TestClass]
+    public class Test_GetFullName
+    {
+        private Challenge challenge;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            challenge = new Challenge();
+        }
+
+        [TestMethod]
+        public void GetFullName_ValidInput_ReturnsFullNameWithSpace()
+        {
+            string result = challenge.GetFullName("Sam", "Joyce");
+            Assert.AreEqual("Sam Joyce", result);
+        }
+    }
+
+    [TestClass]
+    public class Test_IsWithinRange
+    {
+        private Challenge challenge;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            challenge = new Challenge();
+        }
+
+        [TestMethod]
+        public void IsWithinRange_ValidInput_ReturnsTrueWithinRange()
+        {
+            bool result = challenge.IsWithinRange(20, 50);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsWithinRange_ValidInput_ReturnsTrueRangeLimit()
+        {
+            bool result = challenge.IsWithinRange(10, 10);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsWithinRange_ValidInput_ReturnsTrueRangeStart()
+        {
+            bool result = challenge.IsWithinRange(0, 10);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsWithinRange_InvalidInput_ReturnsFalseOutOfRange()
+        {
+            bool result = challenge.IsWithinRange(600, 20);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsWithinRange_InvalidInput_ReturnsFalseBelowZero()
+        {
+            bool result = challenge.IsWithinRange(-50, 20);
+            Assert.IsFalse(result);
+        }
+    }
+
+    [TestClass]
+    public class Test_StringCalculator
+    {
+        private Challenge challenge;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            challenge = new Challenge();
+        }
+
+        [TestMethod]
+        public void StringCalculator_ValidInputs_ReturnsAdditionResult()
+        {
+            int result = challenge.StringCalculator(10, 20, "+");
+            Assert.AreEqual(30, result);
+        }
+
+        [TestMethod]
+        public void StringCalculator_ValidInputs_ReturnsMinusResult()
+        {
+            int result = challenge.StringCalculator(500, 20, "-");
+            Assert.AreEqual(480, result);
+        }
+
+        [TestMethod]
+        public void StringCalculator_ValidInputs_ReturnsDivisionResult()
+        {
+            int result = challenge.StringCalculator(500, 2, "/");
+            Assert.AreEqual(250, result);
+        }
+
+        [TestMethod]
+        public void StringCalculator_ValidInputs_ReturnsMultiplicationResult()
+        {
+            int result = challenge.StringCalculator(5, 20, "*");
+            Assert.AreEqual(100, result);
+        }
+
+        [TestMethod]
+        public void StringCalculator_InvalidInputs_ReturnsMinusOne()
+        {
+            int invalidFirstNumber = challenge.StringCalculator(-500, 2, "/");
+            Assert.AreEqual(-1, invalidFirstNumber);
+            int invalidSecondNumber = challenge.StringCalculator(500, -2, "/");
+            Assert.AreEqual(-1, invalidSecondNumber);
+            int invalidOperator = challenge.StringCalculator(500, -2, "");
+            Assert.AreEqual(-1, invalidOperator);
+        }
+    }
+
+    [TestClass]
+    public class Test_ShouldWakeUp
+    {
+        private Challenge challenge;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            challenge = new Challenge();
+        }
+
+        [TestMethod]
+        public void ShouldWakeUp_ValidInput_ReturnsTrueAfterTenBarking()
+        {
+            bool result = challenge.ShouldWakeUp(true, 23);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ShouldWakeUp_InvalidInput_ReturnsFalseAfterTenNotBarking()
+        {
+            bool result = challenge.ShouldWakeUp(false, 23);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ShouldWakeUp_ValidInput_ReturnsTrueBeforeEightBarking()
+        {
+            bool result = challenge.ShouldWakeUp(true, 5);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ShouldWakeUp_InvalidInput_ReturnsFalseBeforeEightNotBarking()
+        {
+            bool result = challenge.ShouldWakeUp(false, 5);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ShouldWakeUp_ValidInput_ReturnsFalseNotAfterTen()
+        {
+            bool result = challenge.ShouldWakeUp(true, 22);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ShouldWakeUp_ValidInput_ReturnsFalseNotBeforeEight()
+        {
+            bool result = challenge.ShouldWakeUp(true, 8);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ShouldWakeUp_InvalidInput_ReturnsFalseOutOfRange()
+        {
+            bool result = challenge.ShouldWakeUp(true, 50);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ShouldWakeUp_InvalidInput_ReturnsFalseBelowRange()
+        {
+            bool result = challenge.ShouldWakeUp(true, -50);
+            Assert.IsFalse(result);
+        }
+    }
+
+    [TestClass]
+    public class Test_GetMiddleCharacter
+    {
+        private Challenge challenge;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            challenge = new Challenge();
+        }
+
+        [TestMethod]
+        public void GetMiddleCharacter_ValidInput_OddReturnsSingleCharacter()
+        {
+            string result = challenge.GetMiddleCharacter("Sam");
+            Assert.AreEqual("a", result);
+        }
+
+        [TestMethod]
+        public void GetMiddleCharacter_ValidInput_EvenReturnsMultipleCharacters()
+        {
+            string result = challenge.GetMiddleCharacter("Andy");
+            Assert.AreEqual("nd", result);
+        }
+
+        [TestMethod]
+        public void GetMiddleCharacter_InvalidInput_MultipleWordsReturnsInvalidMessage()
+        {
+            string result = challenge.GetMiddleCharacter("Sam Joyce");
+            Assert.AreEqual("Invalid Input", result);
+        }
+
+        [TestMethod]
+        public void GetMiddleCharacter_InvalidInput_NoWordReturnsInvalidMessage()
+        {
+            string result = challenge.GetMiddleCharacter("");
+            Assert.AreEqual("Invalid Input", result);
+        }
+    }
+
+    [TestClass]
+    public class Test_PrintMegaBytesAndKiloBytes
+    {
+        private Challenge challenge;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            challenge = new Challenge();
+        }
+
+        [TestMethod]
+        public void PrintMegaBytesAndKiloBytes_ValidInputs_Returns2MBand172KB()
+        {
+            String result = challenge.PrintMegaBytesAndKiloBytes(2220);
+            Assert.AreEqual("2220 KB = 2 MB and 172 KB", result);
+        }
+
+        [TestMethod]
+        public void PrintMegaBytesAndKiloBytes_ValidInputs_Returns1MBand0KB()
+        {
+            String result = challenge.PrintMegaBytesAndKiloBytes(1024);
+            Assert.AreEqual("1024 KB = 1 MB and 0 KB", result);
+        }
+
+        [TestMethod]
+        public void PrintMegaBytesAndKiloBytes_ValidInputs_Returns0MBand500KB()
+        {
+            String result = challenge.PrintMegaBytesAndKiloBytes(500);
+            Assert.AreEqual("500 KB = 0 MB and 500 KB", result);
+        }
+
+        [TestMethod]
+        public void PrintMegaBytesAndKiloBytes_InvalidInputs_ReturnsInvalidMessage()
+        {
+            String result = challenge.PrintMegaBytesAndKiloBytes(-2220);
+            Assert.AreEqual("Invalid Value", result);
+        }
+    }
+
+    [TestClass]
+    public class Test_CalculateReturn
+    {
+        private Challenge challenge;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            challenge = new Challenge();
+        }
+
+        [TestMethod]
+        public void CalculateReturn_ValidInputs_ReturnGrowth10Years()
+        {
+            string result = challenge.CalculateReturn(1000, 10, 0.1);
+            Assert.AreEqual("You now have " + 2594, result);
+        }
+
+        [TestMethod]
+        public void CalculateReturn_InvalidInputs_InvestmentReturnInvestmentMessage()
+        {
+            string result = challenge.CalculateReturn(-1000, 10, 0.1);
+            Assert.AreEqual("Invalid investment", result);
+        }
+
+        [TestMethod]
+        public void CalculateReturn_InvalidInputs_YearsReturnYearsMessage()
+        {
+            string result = challenge.CalculateReturn(1000, -10, 0.1);
+            Assert.AreEqual("Invalid years", result);
+        }
+
+        [TestMethod]
+        public void CalculateReturn_InvalidInputs_InterestReturnInterestMessage()
+        {
+            string interestToHigh = challenge.CalculateReturn(1000, 10, 1);
+            Assert.AreEqual("Invalid rate of interest", interestToHigh);
+
+            string interestToLow = challenge.CalculateReturn(1000, 10, -0.5);
+            Assert.AreEqual("Invalid rate of interest", interestToLow);
+        }
+    } 
+}
